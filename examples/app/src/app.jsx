0b3343a5cca9972c01dbc74680a4f4beaa599fe6
Vio <vio@beanon.com>
2020-05-08 23:30:02 +0200

    chore(example): Update dependencies

--- examples/app/src/app.jsx
+++ examples/app/src/app.jsx
@@ -4,14 +4,14 @@ import logoUrl from "./img/bundle-stats.png";
 import "./app.css";
 
 export const App = () => (
-	<div className="container">
-		<header className="header">
-			<h1 className="header__title">
-				<img className="header__logo" src={logoUrl} alt="BundleStats" />
-			</h1>
-			<p className="header__description">
-				BundleStats example for custom webpack config
-			</p>
-		</header>
-	</div>
+  <div className="container">
+    <header className="header">
+      <h1 className="header__title">
+        <img className="header__logo" src={logoUrl} alt="BundleStats" />
+      </h1>
+      <p className="header__description">
+        BundleStats example for custom webpack config
+      </p>
+    </header>
+  </div>
 );
--- examples/app/webpack.config.js
+++ examples/app/webpack.config.js
@@ -8,85 +8,85 @@ const CONTEXT = path.join(__dirname, "src");
 const DIST = path.join(__dirname, "dist");
 
 module.exports = (_, { mode }) => ({
-	context: CONTEXT,
-	entry: {
-		app: "./index.jsx"
-	},
-	output: {
-		path: DIST,
-		filename: mode === "production" ? "[name].[contenthash:8].js" : "[name].js"
-	},
-	resolve: {
-		extensions: [".js", ".jsx", ".json"]
-	},
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				loader: "babel-loader",
-				exclude: /node_modules/
-			},
-			{
-				test: /\.css$/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-						options: {
-							hmr: mode === "production"
-						}
-					},
-					"css-loader"
-				]
-			},
-			{
-				test: /\.(jpg|png|gif|webp|svg)$/,
-				loader: "file-loader",
-				options: {
-					name:
-						mode === "production"
-							? "[path][name].[contenthash:8].[ext]"
-							: "[path][name].[ext]"
-				}
-			}
-		]
-	},
-	plugins: [
-		new HtmlPlugin({
-			template: "index.html",
-			filename: "index.html"
-		}),
-		new MiniCssExtractPlugin({
-			filename:
-				mode === "production" ? "[name].[contenthash:8].css" : "[name].css"
-		}),
-		new BundleStatsWebpackPlugin({
-			outDir: "../artifacts",
-			stats: {
-				excludeAssets: [/artifacts/]
-			}
-		}),
-		new StatsWriterPlugin({
-			filename: "../artifacts/webpack-stats.json",
-			stats: {
-				all: true,
-				source: false
-			}
-		})
-	],
-	optimization: {
-		runtimeChunk: {
-			name: "runtime"
-		},
-		splitChunks: {
-			cacheGroups: {
-				vendor: {
-					test: /node_modules/,
-					chunks: "initial",
-					name: "vendor",
-					priority: 10,
-					enforce: true
-				}
-			}
-		}
-	}
+  context: CONTEXT,
+  entry: {
+    app: "./index.jsx",
+  },
+  output: {
+    path: DIST,
+    filename: mode === "production" ? "[name].[contenthash:8].js" : "[name].js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: mode === "production",
+            },
+          },
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.(jpg|png|gif|webp|svg)$/,
+        loader: "file-loader",
+        options: {
+          name:
+            mode === "production"
+              ? "[path][name].[contenthash:8].[ext]"
+              : "[path][name].[ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: "index.html",
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename:
+        mode === "production" ? "[name].[contenthash:8].css" : "[name].css",
+    }),
+    new BundleStatsWebpackPlugin({
+      outDir: "../artifacts",
+      stats: {
+        excludeAssets: [/artifacts/],
+      },
+    }),
+    new StatsWriterPlugin({
+      filename: "../artifacts/webpack-stats.json",
+      stats: {
+        all: true,
+        source: false,
+      },
+    }),
+  ],
+  optimization: {
+    runtimeChunk: {
+      name: "runtime",
+    },
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          chunks: "initial",
+          name: "vendor",
+          priority: 10,
+          enforce: true,
+        },
+      },
+    },
+  },
 });
